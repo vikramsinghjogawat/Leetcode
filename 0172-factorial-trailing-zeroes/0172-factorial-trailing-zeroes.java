3d7d5fe5e7839ad760504b9462d1456d60c100f5
Vikram Singh Jogawat <vikramsinghjogawat5@gmail.com>
2026-07-14 05:00:36 +0530

    Time: 0 ms (100%), Space: 42.3 MB (73.76%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,25 +1,11 @@
 class Solution {
     public int trailingZeroes(int n) {
         if(n==0) return 0;
-        int count1 = 0;
+        int count = 0;
         while(n>0){
-            if(n%5==0){
-                count1++;
-            }
-            if(n%25==0){
-                count1++;
-            }
-            if(n%125==0){
-                count1++;
-            }
-            if(n%625==0){
-                count1++;
-            }
-            if(n%3125==0){
-                count1++;
-            }
-            n--;
+            n=n/5;
+            count+=n;
         }
-        return count1;
+        return count;
     }
 }
