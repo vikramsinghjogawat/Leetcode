d6efc647ccd97c66325f4fd2744f65d1add9a3ed
Vikram Singh Jogawat <vikramsinghjogawat5@gmail.com>
2026-09-18 03:02:01 +0530

    Time: 2 ms (89.42%), Space: 47.7 MB (91.03%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        int goal = nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(maxReach<i) return false;
+            maxReach = Math.max(maxReach,i+nums[i]);
+            if(maxReach>=goal) return true;
+        }
+        return true;
+    }
+}
